9739e244975b90b705bd297b096955f66bd55b14
457030599 <Hs8837!06218>
2019-05-16 15:12:54 +0800

    all

---
+++ html/beam-manage-system/src/components/common/account.js
@@ -0,0 +1,17 @@
+
+import http from '@/util/http'
+
+
+export default {
+    // 修改密码
+    modifyPwd : params => {
+        return http.post("/sys/user/change/password", params)
+    },
+    handleLogout:()=>{
+    	return http.get("/logout", {})
+    },
+    getNavList:()=>{
+        return http.get("/sys/menu/nav", {})
+    }
+
+}
